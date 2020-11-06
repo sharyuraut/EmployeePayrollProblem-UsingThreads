f59b83f0c9a807c48af561f1c18fb5c5e1c93bf8
sharyu raut <sharyukraut210298@gmail.com>
2020-11-06 09:11:04 +0530

    [Sharyu] Add. Initial commit

---
+++ EmployeePayrollUsingThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollUsingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee payroll problem using threads!");
+        }
+    }
+}
